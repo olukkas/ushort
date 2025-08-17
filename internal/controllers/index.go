0f4259ab8b56b3c7bca7a033f84b58bc81a6b809
Lucas Martins dos Santos <lukita.m.s@gmail.com>
2025-08-17 13:16:53 -0300

    wip scaffold tailwindcss and templ for UI

--- internal/controllers/index.go
+++ internal/controllers/index.go
@@ -1,7 +1,10 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/olukkas/ushort/internal/controllers/templates"
+)
 
 func HelloRoute(c *fiber.Ctx) error {
-	return c.SendString("Hello, World!")
+	return templates.Index().Render(c.Context(), c.Response().BodyWriter())
 }
